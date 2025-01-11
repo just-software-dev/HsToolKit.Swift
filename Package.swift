21997602f9dc1029910b97f688b5b15e368d983b
just-software-dev <idontgiveafuck@hotmail.com>
2025-01-11 02:47:03 -0300

    no more web sockets

--- Package.swift
+++ Package.swift
@@ -16,8 +16,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
         .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/just-software-dev/HsExtensions.Swift.git", branch: "main"),
     ],
     targets: [
@@ -26,9 +24,6 @@ let package = Package(
             dependencies: [
                 "Alamofire",
                 "ObjectMapper",
-                .product(name: "NIOSSL", package: "swift-nio-ssl"),
-                .product(name: "NIOWebSocket", package: "swift-nio"),
-                .product(name: "NIOFoundationCompat", package: "swift-nio"),
                 .product(name: "HsExtensions", package: "HsExtensions.Swift"),
             ]
         ),
